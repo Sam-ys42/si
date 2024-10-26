9562d7aa97f05bf665219ae7b8eaed1b9f805547
Sami <ysdelacruzm01@educarex.es>
2024-10-26 23:50:30 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,82 @@
+let randomm = 0
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    if (randomm == 1) {
+        music.setTempo(250)
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(698, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(698, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(698, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(698, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(698, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(698, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        for (let index = 0; index < 4; index++) {
+            music.play(music.tonePlayable(784, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+            music.rest(music.beat(BeatFraction.Sixteenth))
+        }
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(698, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        for (let index = 0; index < 4; index++) {
+            music.play(music.tonePlayable(698, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+            music.rest(music.beat(BeatFraction.Sixteenth))
+        }
+        music.play(music.tonePlayable(698, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        for (let index = 0; index < 3; index++) {
+            music.play(music.tonePlayable(784, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+            music.rest(music.beat(BeatFraction.Sixteenth))
+        }
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(932, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(698, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        for (let index = 0; index < 2; index++) {
+            music.play(music.tonePlayable(698, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+            music.rest(music.beat(BeatFraction.Sixteenth))
+        }
+        music.play(music.tonePlayable(698, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(622, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(784, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        randomm = 0
+    }
+})
